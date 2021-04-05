6c338bd421065f0b39074926820e1139cffeedef
Natalia256 <70845659+Natalia256@users.noreply.github.com>
2021-04-05 12:41:26 +0300

    Add files via upload

---
+++ fireBase.js
@@ -0,0 +1,14 @@
+var fireBase = fireBase || firebase;
+var hasInit = false;
+var config = {
+  apiKey: "AIzaSyD6d_d49VvLn0FGHUVrQK9cnS-jeLg63RQ",
+  authDomain: "proba-ad273.firebaseapp.com",
+  projectId: "proba-ad273",
+  storageBucket: "proba-ad273.appspot.com",
+  messagingSenderId: "716843966889",
+  appId: "1:716843966889:web:b4c76f7a5ce50c3cca64b8"
+  };
+if(!hasInit){
+    firebase.initializeApp(config);
+    hasInit = true;
+}
